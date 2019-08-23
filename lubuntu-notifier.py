4fd6183660bd3b5967f720d122f082f7224edfe2
Hans P Möller <hmoller@uc.cl>
2019-08-22 23:18:02 -0400

    fix changelog date, secure url and other pylint

--- lubuntu-notifier.py
+++ lubuntu-notifier.py
@@ -1,27 +1,50 @@
 #!/usr/bin/python3
+# coding=utf-8
+
+# Copyright (C) 2019 Hans P. Möller <hmollercl@lubuntu.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+''' Open Notification Dialog to enable upgrade'''
 
 import sys
+import subprocess
+from pathlib import Path
+# from optparse import OptionParser
+from argparse import ArgumentParser
+
 from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QPushButton,
                              QHBoxLayout, QVBoxLayout)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from optparse import OptionParser
-from pathlib import Path
-import subprocess
 
 
 class Dialog(QWidget):
+    ''' UI '''
     def __init__(self, upgrades, security_upgrades, reboot_required, upg_path):
         QWidget.__init__(self)
         self.upgrades = upgrades
         self.security_upgrades = security_upgrades
         self.upg_path = upg_path
+        self.reboot_required = reboot_required
 
         self.initUI()
         self.upgradeBtn.clicked.connect(self.call_upgrade)
         self.closeBtn.clicked.connect(self.call_reject)
 
     def initUI(self):
+        ''' UI initialization '''
         self.label = QLabel()
         self.label.setAlignment(Qt.AlignHCenter)
         self.upgradeBtn = QPushButton("Upgrade")
@@ -51,7 +74,7 @@ class Dialog(QWidget):
             text += " upgrade?\nThis will mean packages could be upgraded,"
             text += " installed, or removed."
 
-        if reboot_required:
+        if self.reboot_required:
             if text == "":
                 text = "Reboot is needed"
                 self.upgradeBtn.setVisible(False)
@@ -61,6 +84,7 @@ class Dialog(QWidget):
         self.label.setText(text)
 
     def center(self):
+        ''' puts UI in center of screen '''
         frameGm = self.frameGeometry()
         screen = QApplication.desktop().screenNumber(
             QApplication.desktop().cursor().pos())
@@ -69,9 +93,11 @@ class Dialog(QWidget):
         self.move(frameGm.topLeft())
 
     def call_reject(self):
+        ''' when close button is pressed, quit '''
         app.quit()
 
     def call_upgrade(self):
+        ''' starts upgrade process '''
         self.label.setText("Upgrading...")
         # TODO maybe open another thread so notifier won't freeze
         if self.upg_path == "terminal":
@@ -85,10 +111,6 @@ class Dialog(QWidget):
         self.upgradeBtn.setEnabled(False)
         process = subprocess.Popen(cmd)
         process.wait()
-
-        '''options.fullUpgrade = 1
-        dialogUpg = DialogUpg(optionss, pkg=self.packages)
-        dialogUpg.show()'''
 
         if self.upg_path == "terminal":
             text = "Upgrade finished"
@@ -105,6 +127,7 @@ class Dialog(QWidget):
 
 
 class App(QApplication):
+    '''application'''
     def __init__(self, upgrades, security_upgrades, reboot_required, upg_path,
                  *args):
         QApplication.__init__(self, *args)
@@ -114,6 +137,7 @@ class App(QApplication):
 
 
 def main(args, upgrades, security_upgrades, reboot_required, upg_path):
+    '''main'''
     global app
     app = App(upgrades, security_upgrades, reboot_required, upg_path, args)
     app.setWindowIcon(QIcon.fromTheme("system-software-update"))
@@ -121,30 +145,27 @@ def main(args, upgrades, security_upgrades, reboot_required, upg_path):
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-p",
-                      "--upgrader-sw",
-                      dest="upg_path",
-                      help="Define software/app to open for upgrade",
-                      metavar="APP")
-    parser.add_option("-u",
-                      "--upgrades",
-                      dest="upgrades",
-                      help="How many upgrades are available",
-                      metavar="APP")
-    parser.add_option("-s",
-                      "--security-upg",
-                      dest="security_upgrades",
-                      help="How many security upgrades are available",
-                      metavar="APP")
+    parser = ArgumentParser()
+    parser.add_argument("-p",
+                        "--upgrader-sw",
+                        dest="upg_path",
+                        help="Define software/app to open for upgrade",
+                        metavar="APP")
+    parser.add_argument("-u",
+                        "--upgrades",
+                        dest="upgrades",
+                        help="How many upgrades are available",
+                        metavar="APP")
+    parser.add_argument("-s",
+                        "--security-upg",
+                        dest="security_upgrades",
+                        help="How many security upgrades are available",
+                        metavar="APP")
 
-    (options, args) = parser.parse_args()
+    options = parser.parse_args()
 
     reboot_required_path = Path("/var/run/reboot-required")
-    if reboot_required_path.exists():
-        reboot_required = True
-    else:
-        reboot_required = False
+    reboot_required = reboot_required_path.exists()
 
     if int(options.upgrades) > 0 or reboot_required:
         main(sys.argv, int(options.upgrades), int(options.security_upgrades),
